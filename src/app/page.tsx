09db0b7de08b129c0ba0c401353805b4ef70f5d7
mateolorenzo <mateolorenzo.dev@gmail.com>
2026-01-02 12:37:53 -0300

    Initial clean up and sections distribution

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,66 +1,35 @@
-import Image from "next/image";
 import styles from "./page.module.css";
 
 export default function Home() {
   return (
     <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={100}
-          height={20}
-          priority
-        />
-        <div className={styles.intro}>
-          <h1>To get started, edit the page.tsx file.</h1>
-          <p>
-            Looking for a starting point or more instructions? Head over to{" "}
-            <a
-              href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Templates
-            </a>{" "}
-            or the{" "}
-            <a
-              href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              Learning
-            </a>{" "}
-            center.
-          </p>
-        </div>
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={16}
-              height={16}
-            />
-            Deploy Now
-          </a>
-          <a
-            className={styles.secondary}
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Documentation
-          </a>
-        </div>
-      </main>
+      <section className={styles.section} id="home">
+        <h1>Home</h1>
+      </section>
+
+      <section className={styles.section} id="what-we-do">
+        <h1>What we do</h1>
+      </section>
+
+      <section className={styles.section} id="how-we-work">
+        <h1>How we work</h1>
+      </section>
+
+      <section className={styles.section} id="products-platforms">
+        <h1>Products & Platforms</h1>
+      </section>
+
+      <section className={styles.section} id="our-vision">
+        <h1>Our vision</h1>
+      </section>
+
+      <section className={styles.section} id="lets-build">
+        <h1>Lets build something real</h1>
+      </section>
+
+      <section className={styles.section} id="contact">
+        <h1>Contact us</h1>
+      </section>
     </div>
   );
 }
