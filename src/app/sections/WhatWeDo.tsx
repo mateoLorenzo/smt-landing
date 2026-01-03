7b8df7898b01d68d0ad80f12e30089bc9eb94e48
mateolorenzo <mateolorenzo.dev@gmail.com>
2026-01-03 18:24:01 -0300

    WhatWeDo section responsive improvements

--- src/app/sections/WhatWeDo.tsx
+++ src/app/sections/WhatWeDo.tsx
@@ -1,4 +1,3 @@
-import React from "react";
 import Image from "next/image";
 
 export const WhatWeDo = () => {
@@ -9,30 +8,36 @@ export const WhatWeDo = () => {
     >
       <div className="w-full h-[300px] bg-[#F5F9FA] absolute" />
       {/* Card container with white background */}
-      <div className="relative z-10 w-full max-w-[1500px] mx-[6vw] bg-white rounded-[32px] shadow-2xl py-[10vh] md:px-[100px] md:py-[10vh]">
+      <div className="relative z-10 px-5 w-full max-w-[1500px] mx-[6vw] bg-white rounded-[32px] shadow-2xl py-[10vh] md:px-[100px] md:py-[10vh]">
         {/* Header section */}
         <div className="mb-16">
-          <p className="text-sm uppercase text-[#38777C] mb-4 font-medium">
+          <p className="text-center md:text-left text-sm uppercase text-[#38777C] mb-4 font-medium">
             WHAT WE DO
           </p>
-          {/* <div className="grid md:grid-cols-2 gap-8"> */}
-          <div className="flex justify-between items-center">
+          <div className="flex flex-col text-center md:flex-row md:text-start justify-between items-center">
             {/* <h2 className="text-5xl md:text-4.1xl font-normal text-[#0a3041] leading-tight tracking-[-2px]"> */}
-            <h2 className="text-5xl md:text-[42px] font-normal text-[#0a3041] leading-tight tracking-[-2px]">
-              Software designed to <br /> deliver results
+            <h2 className="text-2xl tracking-[-1px] max-w-[400px] md:text-[42px] font-normal text-[#0a3041] leading-tight md:tracking-[-2px]">
+              Software designed to deliver results
             </h2>
-            <p className="text-base text-[#666666] pt-2">
-              We design and develop digital products with a strong focus <br />{" "}
-              on design, clarity, and real business impact, supported by <br />{" "}
+
+            <p className="pt-4 block md:hidden max-w-[310px] text-xs text-[#666666] md:pt-2">
+              We design and develop digital products with a strong focus on
+              design, clarity, and real business impact, supported by
+              maintainable foundations that grow with your needs.
+            </p>
+
+            <p className="hidden max-w-[460px] md:block text-base text-[#666666] pt-2">
+              We design and develop digital products with a strong focus on
+              design, clarity, and real business impact, supported by
               maintainable foundations that grow with your needs.
             </p>
           </div>
         </div>
 
         {/* Features grid */}
-        <div className="grid md:grid-cols-3 gap-12 mt-20">
+        <div className="grid md:grid-cols-3 gap-12 mt-15 md:mt-20">
           {/* Feature 1 - Product-first mindset */}
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col items-center md:items-start gap-4 ">
             <div className="w-10 h-10 mb-2">
               <Image
                 src="/icons/brain-smt.svg"
@@ -45,14 +50,14 @@ export const WhatWeDo = () => {
             <h3 className="text-xl font-medium text-[#0a3041]">
               Product-first mindset
             </h3>
-            <p className="text-[#666666] text-sm leading-relaxed">
-              We approach every project with a product <br /> mindset focusing
-              on user experience and <br /> business goals from day one.
+            <p className="text-center md:text-start text-[#666666] text-sm leading-relaxed max-w-[290px]">
+              We approach every project with a product mindset focusing 90 user
+              experience and business goals from day one.
             </p>
           </div>
 
           {/* Feature 2 - Scalable architecture */}
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col items-center md:items-start gap-4">
             <div className="w-10 h-10 mb-2">
               <Image
                 src="/icons/scalable.svg"
@@ -65,14 +70,14 @@ export const WhatWeDo = () => {
             <h3 className="text-xl font-medium text-[#0a3041]">
               Scalable architecture
             </h3>
-            <p className="text-[#666666] text-sm leading-relaxed">
-              Every project we handle is built on clean and <br /> solid
-              foundations so scaling later is never a <br /> problem.
+            <p className="text-center md:text-start text-[#666666] text-sm leading-relaxed max-w-[290px]">
+              Every project we handle is built on clean and solid foundations so
+              scaling later is never a problem.
             </p>
           </div>
 
           {/* Feature 3 - Quality & reliability */}
-          <div className="flex flex-col gap-4">
+          <div className="flex flex-col items-center md:items-start gap-4">
             <div className="w-10 h-10 mb-2">
               <Image
                 src="/icons/shield.svg"
@@ -85,9 +90,9 @@ export const WhatWeDo = () => {
             <h3 className="text-xl font-medium text-[#0a3041]">
               Quality & reliability
             </h3>
-            <p className="text-[#666666] text-sm leading-relaxed">
-              Detail-oriented development with a strong <br /> focus on code
-              quality, performance, and <br /> long-term reliability.
+            <p className="text-center md:text-start text-[#666666] text-sm leading-relaxed max-w-[290px]">
+              Detail-oriented development with a strong focus on code quality,
+              performance, and long-term reliability.
             </p>
           </div>
         </div>
